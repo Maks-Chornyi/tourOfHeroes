ce2c588633ff0e7e5245e719c80c5d880ccf78e8
Maks-Chornyi <380960310411@mail.ua>
2018-10-10 16:39:27 +0300

    master: ended sixth part of tourOfHeroes,Services

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Sasha Bartosh' },
+  { id: 12, name: 'Max Turko' },
+  { id: 13, name: 'Hlib Ostapenko'},
+  { id: 14, name: 'Alex Mazur' },
+  { id: 15, name: 'Vova Hlynka' },
+  { id: 16, name: 'Oleh Paliukh' }
+]
+
